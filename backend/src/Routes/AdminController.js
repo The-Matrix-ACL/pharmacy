8e8b8a48634e1da63c8f0307e98ba2a31d9f0108
NinzXtrmz <102371940+NinzXtrmz@users.noreply.github.com>
2023-10-11 13:08:29 +0300

    removed bcrypt

--- backend/src/Routes/AdminController.js
+++ backend/src/Routes/AdminController.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const bcrypt = require("bcrypt");
+//const bcrypt = require("bcrypt");
 const router = express.Router();
 const Admin = require("../Models/Admin.js");
 const Pharmacist = require("../Models/Pharmacist.js"); // Import the Pharmacist model
