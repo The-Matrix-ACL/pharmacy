82aa71a0f3772a9b8bc8dc85c23ab27b673a7b44
AhmedAboelenen99 <130586789+AhmedAboelenen99@users.noreply.github.com>
2023-11-14 13:57:08 -0800

    Add files via upload

--- src/Models/User.js
+++ src/Models/User.js
@@ -1,20 +1,20 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-  Name: {
+const userSchema = new mongoose.Schema({
+  username: {
     type: String,
     required: true,
   },
-  Email: {
+  password: {
     type: String,
-    required: true
-  },
-  Age: {
-    type: Number,
     required: true,
-  }
-}, { timestamps: true });
+  },
+  userType: {
+    type: String,
+    required: true,
+  },
+});
 
 const User = mongoose.model('User', userSchema);
+
 module.exports = User;
